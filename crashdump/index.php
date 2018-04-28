7758b089a94e43db37cde5784c335f7da0c7e033
haniokasai <htek@haniokasai.com>
2018-04-29 00:01:53 +0900

    no message

--- crashdump/index.php
+++ crashdump/index.php
@@ -8,7 +8,7 @@ textdomain(PACKAGE);
 ?>
 <html>
 <head>
-	<title><?php echo _('Crash dump parser'); ?></title>
+	<title><?php echo _('クラッシュダンプ解析機'); ?></title>
 	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 	<link rel="stylesheet" href="/css/main.css" />
 	<script>
@@ -18,24 +18,22 @@ textdomain(PACKAGE);
 	</script>
 </head>
 <body>
-<p><?php echo _('Please choose a method to send the crash dump.'); ?></p>
+<p><?php echo _('送信方法を選択してください。'); ?></p>
 <hr>
 <form action="result.php" method="post" enctype="multipart/form-data">
 	<input type="radio" name="method" value="buffer" checked> <?php echo _('Paste crash dump'); ?>
 	<br>
-	<pre>----------------------<?php echo _('REPORT THE DATA BELOW THIS LINE'); ?>-----------------------
+	<pre>----------------------<?php echo _('レポートデータを下に入力してください'); ?>-----------------------
 ===<?php echo _('BEGIN CRASH DUMP'); ?>===
 <textarea name="buffer" cols="100" rows="30"></textarea>
 ===<?php echo _('END CRASH DUMP'); ?>===
 	</pre>
 	<hr>
-	<input type="radio" name="method" value="file"> <?php echo _('{EXPERIMENTAL} Upload crash dump file'); ?><br>
+	<input type="radio" name="method" value="file"> <?php echo _('{試験的} クラッシュダンプファイルのアップロード'); ?><br>
 	<input type="file" name="file"><br>
 	<hr>
-	<input type="radio" name="method" value="github"> <?php echo _('{EXPERIMENTAL} GitHub link'); ?> <br>
-	<hr>
 	<input type="submit">
-	<input type="checkbox" name="api"> <?php echo _('output as JSON'); ?>
+	<input type="checkbox" name="api"> <?php echo _('JSONとして出力'); ?>
 </form>
 </body>
 </html>
